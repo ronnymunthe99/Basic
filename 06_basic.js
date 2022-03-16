997365efe10ab2ce805828638c68e630ad43ba18
ronnymunthe <ronnymunthe99@gmail.com>
2022-03-16 20:54:22 +0700

    first commit

---
+++ 06_basic.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Belajar Javascript</title>
+</head>
+
+<body>
+<h1>Looping: while</h1>
+
+<script>
+counter = 0;
+while{counter < 5)
+	document.write("Counter: "+counter + "<br />;
+	counter++;
+}
+</script>
+</body>
+</html>
